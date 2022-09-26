464109fd94b8d952934039bdb2decd3af0007ad8
Gunal Sadic <gunal.sadic@totalsoft.ro>
2022-09-26 15:25:57 +0300

    sss

--- src/startup/dataSources.js
+++ src/startup/dataSources.js
@@ -1,15 +1,18 @@
+const PTORequestApi = require('../features/PTORequest/dataSources/PTORequestApi')
 const UserApi = require('../features/user/dataSources/userApi')
 const UserDb = require('../features/user/dataSources/userDb')
 
 module.exports.getDataSources = () => ({
   // Instantiate your data sources here. e.g.: userApi: new UserApi()
   userApi: new UserApi(),
-  userDb: new UserDb()
+  userDb: new UserDb(),
+  PTORequestApi: new PTORequestApi()
 })
 
 module.exports.initializedDataSources = (context, dbInstance, dataSources) => {
   // You need to initialize you datasources here e.g.: dataSources.userApi.initialize({ context })
   dataSources.userApi.initialize({ context })
   dataSources.userDb.initialize({ context: { dbInstance } })
+  dataSources.PTORequestApi.initialize({ context })
   return dataSources
 }
--- src/startup/schema.js
+++ src/startup/schema.js
@@ -8,14 +8,16 @@ const { join } = require('path')
 const userResolvers = require('../features/user/resolvers')
 const userDefs = require('../features/user/schema')
 
+const PTORequestResolver = require('../features/PTORequest/resolvers')
+const PTORequestsDefs = require('../features/PTORequest/schema')
 const oldTypeDefs = []
 const sources = loadTypedefsSync(join(__dirname, '../**/*.graphql'), {
   loaders: [new GraphQLFileLoader()]
 })
 
-const resolvers = merge(userResolvers)
+const resolvers = merge(userResolvers, PTORequestResolver)
 
-const typeDefs = [...sources.map(source => source.document), ...oldTypeDefs, userDefs]
+const typeDefs = [...sources.map(source => source.document), ...oldTypeDefs, userDefs, PTORequestsDefs]
 
 module.exports = makeExecutableSchema({ typeDefs, resolvers })
 module.exports.tests = { typeDefs, resolvers }
