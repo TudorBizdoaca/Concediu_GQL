e7b568b4e11b17e2c5a91e4cc0b9d0e9d8ddfa57
Madalina Mireag <madalina.mireag@totalsoft.ro>
2022-09-27 14:45:05 +0300

    get istoric concedii

--- src/features/holidayHistory/resolvers.js
+++ src/features/holidayHistory/resolvers.js
@@ -2,7 +2,14 @@ const holidayHistoryResolvers = {
   Query: {
     getIstoricConcedii: async (_, { Id }, { dataSources }, _info) => {
       const data = await dataSources.holidayHistoryApi.getIstoricConcedii(Id)
-      return data
+      return data.map(row => {
+        return {
+          ...row,
+          Inlocuitor: row.inlocuitor ? row.inlocuitor.nume + ' ' + row.inlocuitor.prenume : '',
+          Tip: row.tipConcediu ? row.tipConcediu.nume : '',
+          Status: row.stareConcediu ? row.stareConcediu.nume : ''
+        }
+      })
     }
   }
 }
--- src/features/holidayHistory/schema.js
+++ src/features/holidayHistory/schema.js
@@ -2,26 +2,13 @@ const { gql } = require('apollo-server-koa')
 
 const holidayHistoryDefs = gql`
   type HolidayHistory {
-    Id: Int
+    id: Int!
     dataInceput: String!
     dataSfarsit: String!
-    InlocuitorNume: String!
-    InlocuitorPrenume: String!
+    motivRespingere: String
+    Inlocuitor: String!
     Tip: String!
     Status: String!
-    MotivRespingere: String
-  }
-  type Inlocuitor {
-    nume: String
-    prenume: String
-  }
-  type TipConcediu {
-    id: Int
-    nume: String!
-  }
-  type StareConcediu {
-    id: Int
-    nume: String!
   }
 
   extend type Query {
